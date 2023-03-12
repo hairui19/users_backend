a51c5f0c3201b5e45baa12e2f942caa68304baab
hairuilin <linhairui19@gmail.com>
2023-03-12 10:20:38 +0800

    Creates a simple API that returns  at localhost:5000

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 5000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
